9aad41f7331da3e7df3d82143046592a7d738d4e
Jakob Mostert <jakob.mostert@outlook.com>
2020-11-23 14:04:52 +0100

    Submit on Enter (EditText)

--- app/src/main/java/com/example/nesa/MainActivity.java
+++ app/src/main/java/com/example/nesa/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.nesa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nesa.databinding.ActivityMainBinding;
 
@@ -17,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.passwordET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                }
+                return false;
+            }
+        });
+        binding.loginSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
