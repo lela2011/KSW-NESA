137ab50ff8bf430938c3b40c512a71c67954801c
Jakob Mostert <jakob.mostert@outlook.com>
2020-11-23 13:08:49 +0100

    View Binding in MainActivity

--- app/src/main/java/com/example/nesa/MainActivity.java
+++ app/src/main/java/com/example/nesa/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.nesa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.nesa.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
