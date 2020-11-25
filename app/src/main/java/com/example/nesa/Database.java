26af9f0f6ab256442caed43910431dc773625b68
Leon W <lp.wittich@icloud.com>
2020-11-25 14:30:03 +0100

    Database and Repo

--- app/src/main/java/com/example/nesa/Database.java
+++ app/src/main/java/com/example/nesa/Database.java
@@ -1,7 +1,33 @@
 package com.example.nesa;
 
+import android.content.Context;
+
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @androidx.room.Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class Database extends RoomDatabase {
+
+    private static volatile Database instance;
+    public abstract UserDAO userDAO();
+    public static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static Database getInstance(Context context) {
+        if (instance==null) {
+            synchronized (Database.class){
+                if (instance==null){
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            Database.class,
+                            "database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return instance;
+    }
 }
--- app/src/main/java/com/example/nesa/Repository.java
+++ app/src/main/java/com/example/nesa/Repository.java
@@ -6,6 +6,7 @@ public class Repository {
     private UserDAO userDao;
 
     public Repository(Application application) {
-
+        Database database = Database.getInstance(application);
+        userDao = database.userDAO();
     }
 }
