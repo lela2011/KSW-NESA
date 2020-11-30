120891c5b131de380cc7c6f0e1640b5d1b6160c5
Leon W <lp.wittich@icloud.com>
2020-11-30 14:15:27 +0100

    Check internet permission.

--- app/src/main/java/com/example/nesa/LoginActivity.java
+++ app/src/main/java/com/example/nesa/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.example.nesa;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -10,11 +16,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nesa.databinding.LoginActivityBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     LoginActivityBinding binding;
+    AlertDialog.Builder dialogBuilder;
+    public static final int INTERNET_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,30 @@ public class LoginActivity extends AppCompatActivity {
         binding = LoginActivityBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        checkInternetPermission();
+
+        dialogBuilder = new AlertDialog.Builder(LoginActivity.this);
+        dialogBuilder.setMessage(R.string.internet_dialog_message)
+                .setTitle(R.string.internet_dialog_title)
+                .setPositiveButton(R.string.dialogButtonOk, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        requestPermissions(new String[]{Manifest.permission.INTERNET}, INTERNET_REQUEST);
+                    }
+                })
+                .setNegativeButton(R.string.dialogButtonCancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        binding.usernameET.setEnabled(false);
+                        binding.usernameET.setFocusable(false);
+
+                        binding.passwordET.setEnabled(false);
+                        binding.passwordET.setFocusable(false);
+
+                        binding.loginSubmit.setEnabled(false);
+                    }
+                }).create();
 
         binding.passwordET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -46,5 +79,27 @@ public class LoginActivity extends AppCompatActivity {
         String encryptedUsername = AES.encrypt(binding.usernameET.getText().toString(), usernameKey);
         String encryptedPassword = AES.encrypt(binding.passwordET.getText().toString(), passwordKey);
         Toast.makeText(this, "Username:\n" + encryptedUsername + "\nPassword:\n" + encryptedPassword, Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkInternetPermission() {
+        //Log.d("Permission", "Method called");
+        if (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED) {
+            //Log.d("Permission", "Permission denied");
+            if (shouldShowRequestPermissionRationale(Manifest.permission.INTERNET)) {
+                runOnUiThread(() -> {
+                    dialogBuilder.show();
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == INTERNET_REQUEST) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission to internet granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ app/src/main/java/com/example/nesa/LoginHandler.java
@@ -0,0 +1,12 @@
+package com.example.nesa;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class LoginHandler {
+    private static Executor executorService = Executors.newFixedThreadPool(1);
+
+    public int isLoginSuccessful(String username, String password) {
+        return 0;
+    }
+}
