39bb107f92272d2bb03a4527e1c80bfe6a253946
jakob <jakob.mostert@outlook.com>
2021-01-14 18:21:01 +0100

    imports

--- app/src/main/java/com/example/nesa/BankAdapter.java
+++ app/src/main/java/com/example/nesa/BankAdapter.java
@@ -2,7 +2,6 @@ package com.example.nesa;
 
 import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
