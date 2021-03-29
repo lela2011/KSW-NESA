3777b8d2bcdbbfa511c065461367cfecaa4f516b
Leon W <lp.wittich@icloud.com>
2021-03-29 13:13:04 +0200

    Started settingsFragment

--- app/src/main/java/ch/kanti/nesa/fragments/SettingsFragment.java
+++ app/src/main/java/ch/kanti/nesa/fragments/SettingsFragment.java
@@ -4,18 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import ch.kanti.nesa.ColorPickerDialog;
+import ch.kanti.nesa.ViewModel;
 import ch.kanti.nesa.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends Fragment{
+public class SettingsFragment extends Fragment {
 
     public FragmentSettingsBinding binding;
+    public ViewModel viewModel;
 
     @Nullable
     @Override
@@ -27,11 +28,12 @@ public class SettingsFragment extends Fragment{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonColor.setOnClickListener(new View.OnClickListener() {
+        viewModel = new ViewModelProvider(requireActivity()).get(ViewModel.class);
+
+        binding.logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                ColorPickerDialog dialog = new ColorPickerDialog();
-                dialog.show(getChildFragmentManager(), "color");
+            public void onClick(View view) {
+
             }
         });
     }
